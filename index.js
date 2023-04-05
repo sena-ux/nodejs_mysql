75608e682d43e891d2d8060f28dde684ad299ec8
Yansuwitna <yansuwitna@gmail.com>
2023-04-05 16:57:31 +0800

    Latihan Awal

---
+++ index.js
@@ -0,0 +1,44 @@
+//Menempelkan Expressjs
+const express = require('express');
+const app = express();
+
+//Mengkoneksikan Dengan HTML
+const path = require('path')
+const hbs = require('hbs')
+app.set('views', path.join(__dirname))
+app.set('view engine', 'hbs')
+
+//Mengkoneksikan Dengan Database
+const mysql = require('mysql');
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "lsp"
+});
+
+//Routing
+app.get('/',function(r,s){
+    sql = "Select * From tuk";
+    db.query(sql, function(e, qry){
+        s.render('tuk',{
+            judul:'DATA TUK',
+            items:qry
+        });
+    })
+});
+
+app.get('/lsp',function(r,s){
+    sql = "Select * From lsp";
+    db.query(sql, function(e, qry){
+        s.render('lsp',{
+            judul:'DATA LSP',
+            items:qry
+        });
+    })
+});
+
+//Membuat Port Koneksi
+app.listen(1000,function(){
+
+});
